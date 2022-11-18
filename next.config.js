073dc855b7d76e928a2e2eacca3f47f263da624b
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-11-18 09:32:36 +0100

    Deploy project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   swcMinify: true,
+  async rewrites() {
+    console.log("Rewrites called");
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
