69bc54d646cab0c25204139103184123ace2c15f
nguekeu <nguekeu3divin@gmail.com>
2022-12-16 15:23:15 +0100

    Add query fields generation

--- src/services/endpoints/menus.ts
+++ src/services/endpoints/menus.ts
@@ -1,18 +1,44 @@
 import { axiosInstance } from "../axios-instance";
+import { queryFields } from "utils/helpers";
 
 const getMenus = () => {
-  const menus = "id,libelle,display";
-  const categories =
-    "menu_category.categories_id.id,menu_category.categories_id.libelle,menu_category.categories_id.id,menu_category.categories_id.souslibelle";
-  const souscategories =
-    "menu_category.categories_id.souscategories,menu_category.categories_id.souscategories.sousCategories_id.id,menu_category.categories_id.souscategories.sousCategories_id.libelle";
-  const certifications =
-    "menu_certificationCategory.certificationCategories_id.id,menu_certificationCategory.certificationCategories_id.libelle";
-
-  const sousmenus = "sous_menus,sous_menus.id,sous_menus.libelle";
-  return axiosInstance.get(
-    `menus?fields=${menus},${categories},${certifications},${souscategories},${sousmenus}`
-  );
+  const query = {
+    menu: {
+      id: "",
+      libelle: "",
+      display: "",
+      menu_category: {
+        categories_id: {
+          id: "",
+          libelle: "",
+          souslibelle: "",
+          souscategories: {
+            sousCategories_id: {
+              id: "",
+              libelle: "",
+            },
+          },
+        },
+      },
+      menu_certificationCategory: {
+        certificationCategories_id: {
+          id: "",
+          libelle: "",
+          sous_categories: {
+            certificationSousCategories_id: {
+              id: "",
+              libelle: "",
+            },
+          },
+        },
+      },
+      sous_menus: {
+        id: "",
+        libelle: "",
+      },
+    },
+  };
+  return axiosInstance.get(`menus?fields=${queryFields(query)}`);
 };
 
 const getSubMenus = () => {
