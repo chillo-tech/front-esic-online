2fcde117ed41d5495f7c1a703d0e83079125feb9
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-01-22 14:39:58 +0100

    configurations axios

--- src/components/homepage/AvisUtilisateurs.tsx
+++ src/components/homepage/AvisUtilisateurs.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { ApplicationContext } from 'context/ApplicationContext'
+import React, { useContext } from 'react'
 
 function AvisUtilisateurs() {
 
   const {state} = useContext(ApplicationContext);
+  return (      
+    <section className="bg-app-blue text-white bg-no-repeat bg-left bg-contain bg-[url('/images/pages/offers-left-arc.svg')]">
+      rrrrr
+    </section>
+  )
 }
 
 export default AvisUtilisateurs
