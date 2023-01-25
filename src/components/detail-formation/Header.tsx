52619c1c79e5442853da14fb079f393c2220b259
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2023-01-25 14:26:06 +0100

    60% design : fix button header

--- src/components/detail-formation/Header.tsx
+++ src/components/detail-formation/Header.tsx
@@ -3,6 +3,7 @@ import CPFLink from 'components/shared/CPFLink'
 import RenderHtmlContent from 'components/shared/RenderHtmlContent'
 import Link from 'next/link'
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
 import { capitalize, slugify } from 'utils'
 
 function Header({training, toogleDownloadForm}: any) {
@@ -19,6 +20,9 @@ function Header({training, toogleDownloadForm}: any) {
                   <RenderHtmlContent content={training?.contenu} classes='text-white font-light text-lg py-8'/>
                 ) : null
               }
+              <div>
+              
+              </div>
               <div className="flex items-center flex-row gap-2 md:items-end">
                 <AllTrainings 
                   training={training}
@@ -30,7 +34,7 @@ function Header({training, toogleDownloadForm}: any) {
                 {
                    (training.programmepdf ) ? 
                       <button type="button" onClick={toogleDownloadForm}
-                        className="text-white w-full mt-4 mr-2 md:mr-0 md:mt-0 md:px-4 justify-center items-center uppercase py-3 rounded-lg relative border border-app-white hover:bg-white hover:text-app-blue"
+                        className="text-white w-full mt-4 mr-2 md:mr-0 md:mt-0 md:px-4 text-xs md:text-sm justify-center items-center uppercase py-3 rounded-lg relative border border-app-white hover:bg-white hover:text-app-blue"
                       >
                         Je télécharge le programme
                       </button>
