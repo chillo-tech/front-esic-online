3befd433ce3d7bde1961841768e2afea9aa68ac5
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-05-31 09:31:28 +0200

    Scripts google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,7 @@ class AppDocument extends Document {
             <NextScript />
             <noscript>
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQKP99D"
-              height="0" width="0" style="display:none;visibility:hidden">
+              height="0" width="0">
                 </iframe>
             </noscript>
           </body>
