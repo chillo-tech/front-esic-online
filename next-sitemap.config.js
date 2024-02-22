3fc145e03da12a98ce721e883f5c3a1fab353998
Bright Efoo <brightefoo@gmail.com>
2024-02-22 21:41:57 +0100

    update stiemap env variable

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL,
-    changefreq: 'daily',
-    priority: 0.9,
-    sitemapSize: 7000,
-    generateRobotsTxt: true,
-    
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  changefreq: "daily",
+  priority: 0.9,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
 };
